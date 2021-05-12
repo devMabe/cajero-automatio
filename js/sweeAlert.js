20958b3213ec5ed97cf4bb36cf67ee84d7d0c35d
CO5MB <84094099+CO5MB@users.noreply.github.com>
2021-05-12 15:23:46 -0500

    Delete sweeAlert.js

--- js/sweeAlert.js
+++
@@ -1,8 +0,0 @@
-Swal.fire({
-	title: 'Banco Afonca.S.A',
-	html: '<b class="purple">Bienvenido al simulador web</b>',
-	confirmButtonText: '<b class="purple2">Iniciar</b>',
-	confirmButtonColor:  '#5B2C6F'
-
-
-});
